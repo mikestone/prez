51f59084432c0b2d959ec4badfd9552fef4c24ff
Mike Virata-Stone <mike@virata-stone.com>
2015-02-13 17:35:02 -0800

    Getting some initial structure to the presentation.

--- lib/prez/cli.rb
+++ lib/prez/cli.rb
@@ -3,5 +3,16 @@ require "thor"
 module Prez
   class CLI < Thor
     include Thor::Actions
+
+    desc "new NAME", "Generates a new presentation"
+    def new(name)
+      template "new.prez", "#{name}.prez"
+    end
+
+    class << self
+      def source_root
+        File.absolute_path File.expand_path("../../../templates", __FILE__)
+      end
+    end
   end
 end
